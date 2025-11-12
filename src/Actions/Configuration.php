1fb88a06083d7548de9aedd4e90663d51552f218
Brian Austin <brianjaustin@gmail.com>
2025-11-12 20:51:58 +0100

    Mapping config per role

--- src/Actions/Configuration.php
+++ src/Actions/Configuration.php
@@ -4,56 +4,20 @@ namespace OTWSystems\WpOidcLogin\Actions;
 
 use OTWSystems\WpOidcLogin\Utilities\{Render, Sanitize};
 
+use function str_contains;
+use function str_replace;
+
 final class Configuration extends Base
 {
-    /**
-     * @var string
-     */
-    public const CONST_PAGE_GENERAL = 'wp_oidc_login_configuration';
-
     /**
      * @var string
      */
     public const CONST_SECTION_PREFIX = 'wp_oidc_login';
 
     /**
-     * @var array<string, array<string, array<string, array<string, array<string, array<string, array<int, string>|array<string, string>|string>>|array<string, array<string, array<int, string>|array<string, string>|string>|array<string, string|string[]>>|array<string, array<string, array<string, string>|string>>|array<string, array<string, array<string, string>|string|string[]>>|array<string, array<string, string>>|string>>|array<string, array<string, array<string, array<string, array<string, string>|string>>|array<string, array<string, array<string, string>|string|string[]>>|array<string, array<string, string>>|string>>|array<string, array<string, array<string, array<string, array<string, string>|string|string[]>|array<string, string>>|string>>|string>>
+     * @var string
      */
-    private const PAGES = [
-        self::CONST_PAGE_GENERAL => [
-            'title' => 'OIDC Login - Options',
-            'sections' => [
-                'state' => [
-                    'title' => '',
-                    'description' => '',
-                    'options' => [
-                        'enable' => [
-                            'type' => 'boolean',
-                            'enabled' => 'isPluginReady',
-                            'description' => ['getOptionDescription', 'enable'],
-                            'select' => [
-                                'false' => 'Disabled',
-                                'true' => 'Enabled',
-                            ]
-                        ]
-                    ]
-                ],
-                'accounts' => [
-                    'title' => 'WordPress Users Management',
-                    'description' => '',
-                    'options' => [
-                        'default_role' => [
-                            'title' => 'Default Role',
-                            'type' => 'text',
-                            'enabled' => 'isPluginReady',
-                            'description' => 'The role to assign new WordPress users created by the plugin.',
-                            'select' => 'getRoleOptions',
-                        ]
-                    ]
-                ]
-            ]
-        ],
-    ];
+    public const CONST_PAGE_GENERAL = 'wp_oidc_login_configuration';
 
     /**
      * @var array<string, array<int, int|string>|string>
@@ -66,127 +30,184 @@ final class Configuration extends Base
 
     public function onSetup(): void
     {
-        /** @var array<mixed> $page */
-        foreach (self::PAGES as $pageId => $page) {
-            $sections = (isset($page['sections']) && is_array($page['sections'])) ? $page['sections'] : [];
+        /** @var array<string, array<string, array<string, array<string, array<string, array<int, string>|array<string, string>|string>>|array<string, array<string, array<int, string>|array<string, string>|string>|array<string, string|string[]>>|array<string, array<string, array<string, string>|string>>|array<string, array<string, array<string, string>|string|string[]>>|array<string, array<string, string>>|string>>|array<string, array<string, array<string, array<string, array<string, string>|string>>|array<string, array<string, array<string, string>|string|string[]>>|array<string, array<string, string>>|string>>|array<string, array<string, array<string, array<string, array<string, string>|string|string[]>|array<string, string>>|string>>|string> */
+        $sections = [
+            'state' => [
+                'title' => '',
+                'description' => '',
+                'options' => [
+                    'enable' => [
+                        'title' => 'Enable Authentication',
+                        'type' => 'boolean',
+                        'enabled' => 'isPluginReady',
+                        'description' => ['getOptionDescription', 'enable'],
+                        'select' => [
+                            'false' => 'Disabled',
+                            'true' => 'Enabled',
+                        ]
+                    ]
+                ]
+            ],
+            'accounts' => [
+                'title' => 'WordPress Users Management',
+                'description' => '',
+                'options' => []
+            ]
+        ];
 
-            /** @var array<mixed> $section */
-            foreach ($sections as $sectionId => $section) {
-                $sectionId = self::CONST_SECTION_PREFIX . '_' . $sectionId;
-                $sectionType = (isset($section['type']) && is_string($section['type'])) ? $section['type'] : 'array';
-                $sectionCallback = [
-                    $this,
-                    'onUpdate' . str_replace(' ', '', ucwords(str_replace(['wp_oidc_login_', '_'], ' ', $sectionId))),
-                ];
+        /** @var array<string> */
+        foreach ($this->getRoleOptions() as $roleId => $roleName) {
+            $sections['accounts']['options']["mapping_$roleId"] = [
+                'title' => "$roleName Mapping",
+                'type' => 'textarea',
+                'enabled' => 'isPluginReady',
+                'sanitizer' => 'string',
+                'description' => ['getOptionDescription', "{$roleId} mapping"]
+            ];
+        }
 
-                /** @var callable $sectionCallback */
-                register_setting(
-                    option_group: $pageId,
-                    option_name: $sectionId,
+        /** @var array<mixed> $section */
+        foreach ($sections as $sectionId => $section) {
+            $sectionId = self::CONST_SECTION_PREFIX . '_' . $sectionId;
+            $sectionType = (isset($section['type']) && is_string($section['type'])) ? $section['type'] : 'array';
+            $sectionCallback = [
+                $this,
+                'onUpdate' . str_replace(' ', '', ucwords(str_replace(['wp_oidc_login_', '_'], ' ', $sectionId))),
+            ];
+
+            /** @var callable $sectionCallback */
+            register_setting(
+                option_group: self::CONST_PAGE_GENERAL,
+                option_name: $sectionId,
+                args: [
+                    'type' => $sectionType,
+                    'sanitize_callback' => $sectionCallback,
+                    'show_in_rest' => false,
+                ],
+            );
+
+            add_settings_section(
+                id: $sectionId,
+                title: $section['title'],
+                callback: static function () use ($section): void {
+                    echo $section['description'] ?? '';
+                },
+                page: self::CONST_PAGE_GENERAL,
+            );
+
+            /** @var array<string, mixed> $optionValues */
+            $optionValues = get_option($sectionId, []);
+            $options = (isset($section['options']) && is_array($section['options'])) ? $section['options'] : [];
+
+            /** @var array<string, array{title: string, type: string, description?: array<string>|string, placeholder?: array<string>|string, select?: array<mixed>|string, disabled?: bool|string, enabled?: bool|string}> $options */
+            foreach ($options as $optionId => $option) {
+                $elementId = uniqid();
+                $optionType = $option['type'];
+                $optionValue = $optionValues[$optionId] ?? null;
+                $optionName = $sectionId . '[' . $optionId . ']';
+                $optionDescription = $option['description'] ?? '';
+                $optionPlaceholder = $option['placeholder'] ?? '';
+                $optionSelections = $option['select'] ?? null;
+                $optionDisabled = $option['disabled'] ?? null;
+                $optionEnabled = $option['enabled'] ?? null;
+
+                if (is_array($optionDescription)) {
+                    $callback = [$this, $optionDescription[0]];
+
+                    /** @var callable $callback */
+                    $optionDescription = $callback(...array_slice($optionDescription, 1));
+                }
+
+                if (is_array($optionPlaceholder)) {
+                    $callback = [$this, $optionPlaceholder[0]];
+
+                    /** @var callable $callback */
+                    $optionPlaceholder = $callback(...array_slice($optionPlaceholder, 1));
+                }
+
+                if (is_string($optionDisabled)) {
+                    $callback = [$this, $optionDisabled];
+                    /** @var callable $callback */
+                    $optionDisabled = (true === $callback());
+                }
+
+                if (is_string($optionEnabled)) {
+                    $callback = [$this, $optionEnabled];
+                    /** @var callable $callback */
+                    $optionDisabled = (false === $callback());
+                }
+
+                if (is_string($optionSelections)) {
+                    $callback = [$this, $optionSelections];
+                    /** @var callable $callback */
+                    $optionSelections = $callback() ?? [];
+                }
+
+                if (null !== $optionValue && str_contains($optionId, 'mapping')) {
+                    $optionValue = implode("\n", explode(' ', $optionValue));
+                }
+
+                /**
+                 * @var string                      $optionDescription
+                 * @var string                      $optionPlaceholder
+                 * @var null|array<bool|int|string> $optionSelections
+                 * @var bool|int|string             $optionValue
+                 */
+                add_settings_field(
+                    id: $elementId,
+                    title: $option['title'],
+                    callback: static function () use (
+                        $elementId,
+                        $optionName,
+                        $optionType,
+                        $optionDescription,
+                        $optionPlaceholder,
+                        $optionValue,
+                        $optionSelections,
+                        $optionDisabled
+                    ): void {
+                        Render::option(
+                            element: $elementId,
+                            name: $optionName,
+                            type: $optionType,
+                            description: $optionDescription,
+                            placeholder: $optionPlaceholder,
+                            value: $optionValue,
+                            select: $optionSelections,
+                            disabled: $optionDisabled,
+                        );
+                    },
+                    page: self::CONST_PAGE_GENERAL,
+                    section: $sectionId,
                     args: [
-                        'type' => $sectionType,
-                        'sanitize_callback' => $sectionCallback,
-                        'show_in_rest' => false,
+                        'label_for' => $elementId,
+                        'description' => $option['description'] ?? '',
                     ],
                 );
-
-                add_settings_section(
-                    id: $sectionId,
-                    title: $section['title'],
-                    callback: static function () use ($section): void {
-                        echo $section['description'] ?? '';
-                    },
-                    page: $pageId,
-                );
-
-                /** @var array<string, mixed> $optionValues */
-                $optionValues = get_option($sectionId, []);
-                $options = (isset($section['options']) && is_array($section['options'])) ? $section['options'] : [];
-
-                /** @var array<string, array{title: string, type: string, description?: array<string>|string, placeholder?: array<string>|string, select?: array<mixed>|string, disabled?: bool|string, enabled?: bool|string}> $options */
-                foreach ($options as $optionId => $option) {
-                    $elementId = uniqid();
-                    $optionType = $option['type'];
-                    $optionValue = $optionValues[$optionId] ?? null;
-                    $optionName = $sectionId . '[' . $optionId . ']';
-                    $optionDescription = $option['description'] ?? '';
-                    $optionPlaceholder = $option['placeholder'] ?? '';
-                    $optionSelections = $option['select'] ?? null;
-                    $optionDisabled = $option['disabled'] ?? null;
-                    $optionEnabled = $option['enabled'] ?? null;
-
-                    if (is_array($optionDescription)) {
-                        $callback = [$this, $optionDescription[0]];
-
-                        /** @var callable $callback */
-                        $optionDescription = $callback(...array_slice($optionDescription, 1));
-                    }
-
-                    if (is_array($optionPlaceholder)) {
-                        $callback = [$this, $optionPlaceholder[0]];
-
-                        /** @var callable $callback */
-                        $optionPlaceholder = $callback(...array_slice($optionPlaceholder, 1));
-                    }
-
-                    if (is_string($optionDisabled)) {
-                        $callback = [$this, $optionDisabled];
-                        /** @var callable $callback */
-                        $optionDisabled = (true === $callback());
-                    }
-
-                    if (is_string($optionEnabled)) {
-                        $callback = [$this, $optionEnabled];
-                        /** @var callable $callback */
-                        $optionDisabled = (false === $callback());
-                    }
-
-                    if (is_string($optionSelections)) {
-                        $callback = [$this, $optionSelections];
-                        /** @var callable $callback */
-                        $optionSelections = $callback() ?? [];
-                    }
-
-                    /**
-                     * @var string                      $optionDescription
-                     * @var string                      $optionPlaceholder
-                     * @var null|array<bool|int|string> $optionSelections
-                     * @var bool|int|string             $optionValue
-                     */
-                    add_settings_field(
-                        id: $elementId,
-                        title: $option['title'],
-                        callback: static function () use (
-                            $elementId,
-                            $optionName,
-                            $optionType,
-                            $optionDescription,
-                            $optionPlaceholder,
-                            $optionValue,
-                            $optionSelections,
-                            $optionDisabled
-                        ): void {
-                            Render::option(
-                                element: $elementId,
-                                name: $optionName,
-                                type: $optionType,
-                                description: $optionDescription,
-                                placeholder: $optionPlaceholder,
-                                value: $optionValue,
-                                select: $optionSelections,
-                                disabled: $optionDisabled,
-                            );
-                        },
-                        page: $pageId,
-                        section: $sectionId,
-                        args: [
-                            'label_for' => $elementId,
-                            'description' => $option['description'] ?? '',
-                        ],
-                    );
-                }
             }
         }
+    }
+
+    /**
+     * @param null|array<null|bool|int|string> $input
+     *
+     * @return null|array<mixed>
+     */
+    public function onUpdateAccounts(?array $input): ?array
+    {
+        if (null === $input) {
+            return null;
+        }
+
+        $sanitized = [];
+
+        foreach ($this->getRoleOptions() as $roleId => $roleName) {
+            $mappings = Sanitize::textarea((string) ($input["mapping_$roleId"] ?? '')) ?? '';
+            $mappings = explode("\n", $mappings);
+            $sanitized["mapping_$roleId"] = trim(implode(' ', Sanitize::arrayUnique($mappings)));
+        }
+
+        return array_filter($sanitized, static fn($value): bool => '' !== $value);
     }
 
     /**
@@ -224,7 +245,7 @@ final class Configuration extends Base
 
     public function renderConfiguration(): void
     {
-        Render::pageBegin(self::PAGES[self::CONST_PAGE_GENERAL]['title']);
+        Render::pageBegin('WP OIDC Login - Options');
 
         settings_fields(self::CONST_PAGE_GENERAL);
         do_settings_sections(self::CONST_PAGE_GENERAL);
@@ -235,12 +256,18 @@ final class Configuration extends Base
 
     private function getOptionDescription(string $context): string
     {
-        if ('enable' === $context) {
-            if ($this->isPluginReady()) {
-                return 'Manage WordPress authentication with OIDC.';
-            }
-
+        if (!$this->isPluginReady()) {
             return 'Plugin requires configuration.';
+        }
+
+        if ('enable' === $context) {
+            return 'Manage WordPress authentication with OIDC.';
+        }
+
+        if (str_contains($context, 'mapping')) {
+            /** @var string */
+            $roleId = str_replace($context, ' mapping', '');
+            return "A list of groups to map to the $roleId role, each on its own line.";
         }
     }
 
@@ -259,6 +286,6 @@ final class Configuration extends Base
         }
 
         /** @var string[] $response */
-        return array_reverse($response, true);
+        return $response;
     }
 }
